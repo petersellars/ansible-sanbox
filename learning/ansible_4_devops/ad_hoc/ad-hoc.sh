da5401c167a91f62fbb3e775e46f0b7ece1e8585
Pete Sellars <psellars@gmail.com>
2016-10-11 22:46:42 +1300

    update ad-hoc commands to add database and limits, add some Vagrantfile changes

--- learning/ansible_4_devops/ad_hoc/ad-hoc.sh
+++ learning/ansible_4_devops/ad_hoc/ad-hoc.sh
@@ -23,3 +23,21 @@ ansible -i hosts app -s -m yum -a "name=python-setuptools state=present"
 ansible -i hosts app -s -m easy_install -a "name=django state=present"
 
 ansible -i hosts app -a "python -c 'import django; print django.get_version()'"
+
+# Configure Database servers
+ansible -i hostsansible -i hosts db -s -m yum -a "name=mariadb-server state=present"
+ansible -i hosts db -s -m service -a "name=mariadb state=started enabled=yes"
+ansible -i hosts db -s -a "iptables -F"
+ansible -i hosts db -s -a "iptables -A INPUT -s 192.168.60.0/24 -p tcp \
+ -m tcp --dport 3306 -j ACCEPT"
+ansible -i hosts db -s -m yum -a "name=MySQL-python state=present"
+ansible -i hosts db -s -m mysql_user -a "name=django host=% password=12345 \
+ priv=*.*:ALL state=present"
+
+# Operating on one server
+ansible -i hosts app -s -a "service ntpd status"
+ansible -i hosts app -s -a "service ntpd restart" --limit "192.168.60.4"
+# Limit hosts with a simple pattern (asterisk is a wildcard)
+ansible -i hosts app -s -a "service ntpd restart" --limit "*.4"
+# Limit hosts with a regular expression (prefix with a tilde)
+ansible -i hosts app -s -a "service ntpd restart" --limit ~".*\.4"
